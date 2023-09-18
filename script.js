beab44ea114127257a57117e96a4b8aa1179b8b9
Abhishek Kumar <abhi24033c@gmail.com>
2023-09-18 18:27:38 +0530

    script file added

---
+++ script.js
@@ -0,0 +1,13 @@
+// const bars=document.getElementById('bars');
+
+function barClick(){
+    const menu=document.getElementById('ul');
+    menu.style.display="flex"
+    menu.style.right="0px";
+    console.log("helo")
+}
+function crossClick(){
+    const menu=document.getElementById("ul");
+    menu.style.right="-40rem"
+    menu.style.display="none";
+}
